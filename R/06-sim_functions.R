e5602eff2da9d743d982ee3d09d1d731b0f533e5
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2023-04-16 06:54:21 +0800

    Change starting seed sims

--- R/06-sim_functions.R
+++ R/06-sim_functions.R
@@ -45,7 +45,7 @@ globalVariables(c("i"))
 #' }
 run_ligof_sims <- function(model_no = 1, nsim = 1000, samp_size = 1000,
                        samp = c("srs", "strat", "clust", "strcl"),
-                       simtype = c("type1", "power"), starting_seed = 4423,
+                       simtype = c("type1", "power"), starting_seed = 16423,
                        ncores = parallel::detectCores() - 2) {
 
   # Model setup
