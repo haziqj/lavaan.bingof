d9504a91d09b40c6dc3a22b056b85cf51f9984b6
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2023-07-27 09:12:57 -0400

    Update simulations

--- data-raw/07-run_simulations.R
+++ data-raw/07-run_simulations.R
@@ -4,17 +4,17 @@ library(survey)
 analysis_path <- dirname(rstudioapi::getSourceEditorContext()$path)
 
 # All simulations --------------------------------------------------------------
-for (sim_type in c("power")) {
-  for (samp_method in c("srs", "strat", "clust", "strcl")) {
+for (sim_type in c("type1", "power")) {
+  for (samp_method in c("strat", "clust", "strcl")) {
     for (the_samp_size in c(500, 1000, 2000, 3000)) {
-      for (mod_no in c(2, 3)) {
+      for (mod_no in 1:5) {
         sim_name <- paste0(samp_method, mod_no, "_n", the_samp_size, "_",
                            sim_type)
         cat("[", as.character(Sys.time()), "]", "Now running simulation",
             sim_name, "\n")
-        sim <- run_ligof_sims(mod_no, samp_size = the_samp_size, nsim = 1000,
+        sim <- run_ligof_sims(mod_no, samp_size = the_samp_size, nsim = 250,
                               samp = samp_method, simtype = sim_type,
-                              pop_Sigma = FALSE, bootstrap = FALSE)
+                              pop_Sigma = TRUE, bootstrap = FALSE)
         invisible(list2env(setNames(list(sim), sim_name), envir = .GlobalEnv))
         save(list = sim_name, file = paste0(analysis_path, "/Rsave/",
                                             sim_name, ".RData"))
