b6a96decf7a7e346861a189cce2ed9055f0df26c
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2023-08-05 12:24:10 +0800

    Update article

--- data-raw/08-analyse_sims.R
+++ data-raw/08-analyse_sims.R
@@ -84,14 +84,45 @@ srs_plot <- function(x = res_srs_type1, alpha = 10, dashed_line = TRUE,
            .(plot_title)~"("*alpha~"="~.(iprior::dec_plac(alpha/100, 2))*")"
          ))) +
     guides(col = guide_legend(ncol = 1), shape = guide_legend(ncol = 1)) +
-    scale_colour_viridis_d(option = "turbo", direction = -1) +
-    scale_fill_viridis_d(option = "turbo", direction = -1)
-    # scale_color_jcolors()
+    # scale_colour_viridis_d(option = "turbo", direction = -1) +
+    # scale_fill_viridis_d(option = "turbo", direction = -1) +
+    jcolors::scale_colour_jcolors(palette = "pal8") +
+    jcolors::scale_fill_jcolors(palette = "pal8")
 }
 
-p_srs_a <- srs_plot(res_srs_type1, alpha = 10)
-p_srs_b <- srs_plot(res_srs_type1, alpha = 5)
-p_srs_c <- srs_plot(res_srs_type1, alpha = 1)
+srs_plot2 <- function(x = res_srs_type1, alpha = 5, dashed_line = TRUE,
+                      plot_title = "Type I errors",
+                      exclude_tests = c("RSS,MM3", "Multn,MM3")) {
+  var_name <- paste0("rej_rate", alpha)
+  crit_name <- paste0("crit", alpha)
+
+  x <- x %>%
+    filter(!name %in% exclude_tests) %>%
+    mutate(sim = factor(sim, labels = c("1*F~5*V", "1*F~8*V", "1*F~15*V",
+                                        "2*F~10*V", "3*F~15*V")),
+           name = factor(name, levels = rev(levels(name))),
+           ok = !(!is.na(.data[[var_name]]) & !(abs(.data[[var_name]] - alpha / 100) < .data[[crit_name]])),
+           N = factor(n))
+
+  p <- ggplot(x, aes(.data[[var_name]], name, col = N, shape = N, alpha = ok)) +
+    geom_vline(aes(xintercept = alpha / 100), linetype = "dashed") +
+    geom_pointrange(aes(xmin = .data[[var_name]] - .data[[crit_name]],
+                        xmax = .data[[var_name]] + .data[[crit_name]]),
+                    position = position_dodge(width = 0.5)) +
+    facet_grid(. ~ sim, labeller = label_parsed) +
+    jcolors::scale_colour_jcolors(palette = "pal8") +
+    scale_alpha_manual(values = c(0.4, 1)) +
+    # scale_x_continuous(breaks = alpha / 100) +
+    scale_shape_manual(values = c(16, 17, 15, 4, 1, 2)) +
+    labs(x = "Rejection rate", y = NULL, alpha = "Within\n95% interval",
+         shape = "Sample size", col = "Sample size") +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  p
+}
+
+p_srs_a <- srs_plot2(res_srs_type1, alpha = 10)
+p_srs_b <- srs_plot2(res_srs_type1, alpha = 5)
+p_srs_c <- srs_plot2(res_srs_type1, alpha = 1)
 p_srs_d <- srs_plot(res_srs_power, alpha = 10, dashed_line = FALSE,
                     plot_title = "Power")
 p_srs_e <- srs_plot(res_srs_power, alpha = 5, dashed_line = FALSE,
@@ -180,10 +211,56 @@ p_complex_f <- complex_plot(res_complex_power, alpha = 1, dashed_line = FALSE,
 usethis::use_data(res_complex_type1, overwrite = TRUE)
 usethis::use_data(res_complex_power, overwrite = TRUE)
 
+
+# Distribution of test statistics ----------------------------------------------
+plot_X2_dens <- function(samp = "strcl") {
+  dat <- grab_sims(samp = samp)
+  plot_df2 <-
+    dat %>%
+    group_by(name, sim) %>%
+    summarise(mdf = mean(df),
+              sdf = sd(df),
+              minx = min(X2),
+              maxx = max(X2),
+              .groups = "drop") %>%
+    rename(df = mdf) %>%
+    rowwise() %>%
+    mutate(xx = list(seq(minx, maxx, length = 200)),
+           yy = list(dchisq(xx, df = df))) %>%
+    unnest_longer(col = c(xx, yy))
+
+
+  dat %>%
+    mutate(n = paste0("n = ", n),
+           n = factor(n, levels = paste0("n = ", c(500, 1000, 2000, 3000, 5000,
+                                                   10000)))) %>%
+    ggplot(aes(X2, y = stat(density), group = n, fill = n)) +
+    geom_histogram(position = "identity", alpha = 0.5, col = NA, bins = 30) +
+    # geom_density(alpha = 0.5) +
+    # geom_freqpoly() +
+    geom_line(data = plot_df2, aes(xx, yy), col = "black", inherit.aes = FALSE) +
+    facet_wrap(name ~ sim, scales = "free", ncol = 5) +
+    jcolors::scale_colour_jcolors(palette = "pal7") +
+    jcolors::scale_fill_jcolors(palette = "pal7") +
+    theme(legend.position = "bottom") +
+    guides(fill = guide_legend(nrow = 1))
+}
+
+p_hist_a <- plot_X2_dens("srs")
+p_hist_b <- plot_X2_dens("strat")
+p_hist_c <- plot_X2_dens("clust")
+p_hist_d <- plot_X2_dens("strcl")
+
+# dat %>%
+#   ggplot(aes(x = 1, y = X2, col = name)) +
+#   geom_boxplot() +
+#   facet_grid(sim ~ n, scales = "free")
+
 # Save plots -------------------------------------------------------------------
 save(p_srs_a, p_srs_b, p_srs_c,
      p_srs_d, p_srs_e, p_srs_f,
      p_complex_a, p_complex_b, p_complex_c,
      p_complex_d, p_complex_e, p_complex_f,
      srs_plot, complex_plot,
+     p_hist_a, p_hist_b, p_hist_c, p_hist_d,
      file = "vignettes/articles/simplots.RData")
