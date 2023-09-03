d9fbc2a966e3d1e0114547a1fbee13d91e6c07c8
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2023-09-03 09:27:39 +0800

    Update informative sampling data gen

--- R/32-data_gen_weighted.R
+++ R/32-data_gen_weighted.R
@@ -10,20 +10,25 @@
 #'
 #' @examples
 #' gen_data_bin_wt(1)
-gen_data_bin_wt <- function(model_no = 1, seed = 123, H1 = FALSE,
-                            return_all = FALSE, N = 1000, n) {
+gen_data_bin_wt <- function(model_no = 1, seed = NULL, H1 = FALSE,
+                            return_all = FALSE, n = 1000) {
+  # if (!missing(n)) N <- n * 10
+  res <-
+    gen_data_bin(model_no = model_no, n = n * 10, seed = 1923, H1 = H1,
+                 return_all = TRUE)
 
-  if (!missing(n)) N <- n * 2
+  set.seed(seed)
 
   res <-
-    gen_data_bin(model_no = model_no, n = N, seed = seed, H1 = H1,
-                 return_all = TRUE) %>%
+    res %>%
     select(-dplyr::contains("ystar")) %>%
-    mutate(prob = rowSums(across(starts_with("eta"))),
-           prob = 1 / (1 + exp(prob))) %>%
-    mutate(sampled = stats::rbinom(dplyr::n(), size = 1, prob = prob) %>%
-             as.logical()) %>%
-    filter(.data$sampled) %>%
+    mutate(prob = 2 * rowSums(across(starts_with("eta"))),
+           prob = exp(prob) / (1 + exp(prob))) %>%
+    slice_sample(n = n, weight_by = prob, replace = TRUE) %>%
+    # NOTE TO SELF: weight_by is actually probability of selection (sums to 1)
+    # mutate(sampled = stats::rbinom(dplyr::n(), size = 1, prob = prob) %>%
+    #          as.logical()) %>%
+    # filter(.data$sampled) %>%
     mutate(wt = 1 / prob,
            wt = dplyr::n() * wt / sum(wt))
 
@@ -34,4 +39,3 @@ gen_data_bin_wt <- function(model_no = 1, seed = 123, H1 = FALSE,
       select(wt, starts_with("y"))
   }
 }
-
--- tests/testthat/_snaps/test_statistics_weighted.md
+++ tests/testthat/_snaps/test_statistics_weighted.md
@@ -4,12 +4,12 @@
       res
     Output
       # A tibble: 6 x 6
-           X2    df name          pval Xi_rank     S
-        <dbl> <dbl> <chr>        <dbl>   <int> <int>
-      1  3.92  5    Wald         0.562      13    15
-      2  3.84  5    WaldVCF      0.573       5    15
-      3  1.80  3.50 WaldDiag,MM3 0.702      15    15
-      4  2.46  4.36 WaldDiag,RS2 0.704      15    15
-      5  2.72  3.50 Pearson,MM3  0.525      15    15
-      6  3.51  4.39 Pearson,RS2  0.535      15    15
+             X2    df name          pval Xi_rank     S
+          <dbl> <dbl> <chr>        <dbl>   <int> <int>
+      1  1.60    5    Wald         0.902      12    15
+      2  0.670   5    WaldVCF      0.985       5    15
+      3  0.143   2.39 WaldDiag,MM3 0.963      15    15
+      4  1.24    4.23 WaldDiag,RS2 0.892      15    15
+      5 -0.0172  1.26 Pearson,MM3  1          15    15
+      6  0.548   2.25 Pearson,RS2  0.810      15    15
 
